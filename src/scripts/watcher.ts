ba8e9d651b109d963eb4bc410ff62b436cc4dd77
unel <unel@bk.ru>
2022-06-22 09:51:33 +0300

    some progress in watcher, huh? (=

--- src/scripts/watcher.ts
+++ src/scripts/watcher.ts
@@ -1,19 +1,24 @@
 import { resolve } from 'path';
 import chokidar from 'chokidar';
 
+import { computeFileHash } from "$utils/file-utils/compute-file-hash";
 import env from '$constants/env';
 
 function main() {
 	console.log('started with env', env);
 	const path = resolve(env.UPLOAD_ROOT);
 	const watcher = chokidar.watch(path, {
-		ignoreInitial: true,
+		ignoreInitial: false,
 	});
 
 	watcher.on('add', (path) => {
-		console.log('new file added', path);
+		processFile(path);
 	})
 }
 
+async function  processFile(path: string) {
+	const hash = await computeFileHash(path);
+	console.log('new file added', path, hash);
+}
 
 main();
